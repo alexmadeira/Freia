b12a6a7581eef05156d6f8411ad788659d6a851b
Alex Madeira <>
2018-09-02 15:18:49 -0300

    basico

--- start/routes.js
+++ start/routes.js
@@ -1,20 +1,9 @@
 'use strict'
 
-/*
-|--------------------------------------------------------------------------
-| Routes
-|--------------------------------------------------------------------------
-|
-| Http routes are entry points to your web application. You can create
-| routes for different URL's and bind Controller actions to them.
-|
-| A complete guide on routing is available here.
-| http://adonisjs.com/docs/4.0/routing
-|
-*/
-
 const Route = use('Route')
 
-Route.get('/', ({ request }) => {
-  return { greeting: 'Hello world in JSON' }
-})
+Route.post('users', 'UserController.store')
+Route.post('sessions', 'SessionController.store')
+
+Route.post('files', 'FileController.store')
+Route.get('files/:id', 'FileController.show')
