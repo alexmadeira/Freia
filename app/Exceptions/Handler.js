db1044d312eacd7a62b1240c87748743b5a30495
Alex Madeira <alex.madeira@atomocom.com.br>
2018-10-31 09:24:45 -0300

    teste yout

--- app/Exceptions/Handler.js
+++ app/Exceptions/Handler.js
@@ -3,7 +3,7 @@ const Sentry = require('@sentry/node')
 
 const Config = use('Config')
 const Env = use('Env')
-const Youch = use('Youch')
+const Youch = use('youch')
 const BaseExceptionHandler = use('BaseExceptionHandler')
 
 class ExceptionHandler extends BaseExceptionHandler {
