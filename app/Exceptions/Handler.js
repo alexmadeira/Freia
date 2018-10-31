321ba4877f09c61c704db52ff48a56b2ef1b86c5
Alex Madeira <alex.madeira@atomocom.com.br>
2018-10-31 09:28:05 -0300

    reverse tests

--- app/Exceptions/Handler.js
+++ app/Exceptions/Handler.js
@@ -13,10 +13,9 @@ class ExceptionHandler extends BaseExceptionHandler {
       return response.status(error.status).send(error.message)
     }
 
-    if (Env.get('NODE_ENV') !== 'development') {
+    if (Env.get('NODE_ENV') === 'development') {
       const youch = new Youch(error, request.request)
       const errorJSON = await youch.toJSON()
-      console.log(errorJSON)
       return response.status(error.status).send(errorJSON)
     }
 
