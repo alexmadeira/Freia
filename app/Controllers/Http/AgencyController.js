81f910ba31a8e48ed0783d5c70ca84dfcac7f2bf
iMac20 <imac20@iMac20.local>
2019-02-20 16:59:41 -0300

    get agencye

--- app/Controllers/Http/AgencyController.js
+++ app/Controllers/Http/AgencyController.js
@@ -4,13 +4,15 @@ const Agency = use('App/Models/Agency')
 class AgencyController {
   async index ({ request }) {
     const { order } = request.get()
-    const arrayOrder = order.split(':')
 
     const agencyQuery = Agency.query()
       .with('image')
       .with('logo')
 
-    if (order) agencyQuery.orderBy(arrayOrder[0], arrayOrder[1])
+    if (order) {
+      const arrayOrder = order.split(':')
+      agencyQuery.orderBy(arrayOrder[0], arrayOrder[1])
+    }
 
     const agency = await agencyQuery.fetch()
 
@@ -32,6 +34,7 @@ class AgencyController {
 
     return agency
   }
+
   async update ({ params, request }) {
     const agency = await Agency.findOrFail(params.id)
     const data = request.only([
@@ -47,6 +50,16 @@ class AgencyController {
 
     agency.merge(data)
     await agency.save()
+
+    return agency
+  }
+
+  async show ({ params }) {
+    const { id } = params
+    const agency = await Agency.findByOrFail('id', id)
+
+    await agency.load('image')
+    await agency.load('logo')
 
     return agency
   }
